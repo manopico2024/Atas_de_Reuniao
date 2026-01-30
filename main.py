067ee3a49ab3815cdbb5afb42933cc6645319673
MANO <mano.pico2021@gmail.com>
2026-01-30 19:34:33 -0300

    first commit

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ from PyQt5.QtWidgets import (QDialog, QMessageBox, QMainWindow, QApplication,
                              QProgressDialog, QWidget, QTreeWidget, QLineEdit)
 from PyQt5.QtCore import Qt, QCoreApplication
 from PyQt5.QtGui import QKeySequence, QIcon, QPixmap
-from PyQt5.QtCore import Qt, QCoreApplication, QPoint, QTimer
+from PyQt5.QtCore import Qt, QCoreApplication, QPoint
 from PyQt5.QtGui import QCursor
+import requests
 import pandas as pd
 import sqlite3
 from pathlib import Path
@@ -33,19 +34,15 @@ import random
 from models.telaLogin import Ui_telaLogin
 from models.telaGraficodePresenca import Ui_telaGraficodePresenca
 from models.telaPrincipal import Ui_telaPrincipal
-
+from models.telaCatalogoBiblico import Ui_telaCatalogoBiblico
+from biblia_api import BibliaDigitalAPI
 
 class TelaArrastavelBase(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self._offset = None
-
-        self.setWindowFlags(
-            Qt.FramelessWindowHint |
-            Qt.Window
-        )
-
-        # Janela sem bordas
+        
+        # Configurar janela sem bordas
         self.setWindowFlags(Qt.FramelessWindowHint)
 
     def mousePressEvent(self, event):
@@ -70,12 +67,6 @@ class Login(TelaArrastavelBase):
         self.ui = Ui_telaLogin()
         self.ui.setupUi(self)
 
-        self._offset = None
-
-        self.setWindowFlags(
-            Qt.FramelessWindowHint |
-            Qt.Window
-        )
         self.setWindowIcon(QIcon("icons/icons/logoSpike.png"))
 
         # Configurar campos de senha
@@ -161,19 +152,13 @@ class Login(TelaArrastavelBase):
             QCoreApplication.quit()
         event.accept()
 
-
 class TelaGraficodePresenca(TelaArrastavelBase):
     def __init__(self, parent=None):
         super().__init__(parent)  # Chama o construtor da classe base
         self.ui = Ui_telaGraficodePresenca()
         self.ui.setupUi(self)
-        self._offset = None
-
-        self.setWindowFlags(
-            Qt.FramelessWindowHint |
-            Qt.Window
-        )
-         # Criar diretório para arquivos
+        
+        # Criar diretório para arquivos
         os.makedirs("arquivo", exist_ok=True)       
         # Configurações da janela
         self.setWindowIcon(QIcon("icons/icons/logoSpike.png"))
@@ -181,15 +166,94 @@ class TelaGraficodePresenca(TelaArrastavelBase):
         
         print("✅ Tela Gráfico de Presença criada com arrasta e solta!")
 
-
-class AtasdeReuniao(TelaArrastavelBase):
+class CatalogoBiblico(TelaArrastavelBase):
     def __init__(self, parent=None):
         super().__init__(parent)  # Chama o construtor da classe base
+        self.ui = Ui_telaCatalogoBiblico()
+        self.ui.setupUi(self)
+        
+        self.setWindowIcon(QIcon("icons/icons/logoSpike.png"))
+        self.setGeometry(150, 200, 973, 437)
+        
+        # Criar diretório para arquivos
+        os.makedirs("arquivo", exist_ok=True)
+
+        self.api = BibliaDigitalAPI()
+
+        # Conectar botões
+        self.ui.btnCarregarLivros.clicked.connect(self.carregar_livros)
+        self.ui.btnBuscarCapitulo.clicked.connect(self.carregar_capitulo)
+
+        # Carregar livros automaticamente
+        self.carregar_livros()
+        
+    def carregar_livros(self):
+        try:
+            livros = self.api.get_books()
+
+            self.ui.comboLivros.clear()
+            for livro in livros:
+                self.ui.comboLivros.addItem(
+                    livro["name"],
+                    livro["abbrev"]["pt"]
+                )
+
+            print("📚 Livros carregados com sucesso")
+
+        except Exception as e:
+            QMessageBox.critical(
+                self,
+                "Erro",
+                f"Erro ao carregar livros:\n{str(e)}"
+            )
+    
+    def carregar_capitulo(self):
+        try:
+            abbrev = self.ui.comboLivros.currentData()
+            capitulo = self.ui.spinCapitulo.value()
+            versao = self.ui.comboVersao.currentText()
+
+            dados = self.api.get_chapter(versao, abbrev, capitulo)
+
+            self.ui.listaVersiculos.clear()
+
+            for verso in dados["verses"]:
+                texto = f'{verso["number"]} - {verso["text"]}'
+                self.ui.listaVersiculos.addItem(texto)
+
+            print("📖 Capítulo carregado")
+
+        except Exception as e:
+            QMessageBox.critical(
+                self,
+                "Erro",
+                f"Erro ao carregar capítulo:\n{str(e)}"
+            )
+    
+    def versiculo_aleatorio(self):
+        try:
+            versao = self.ui.comboVersao.currentText()
+            verso = self.api.get_random_verse(versao)
+
+            texto = (
+                f'{verso["book"]["name"]} '
+                f'{verso["chapter"]}:{verso["number"]}\n\n'
+                f'{verso["text"]}'
+            )
+
+            self.ui.txtResultado.setPlainText(texto)
+
+        except Exception as e:
+            QMessageBox.critical(self, "Erro", str(e))
+
+class AtasdeReuniao(TelaArrastavelBase):
+    def __init__(self):
+        super().__init__()  # Não passa parent aqui
+        
         self.ui = Ui_telaPrincipal()
         self.ui.setupUi(self)
         
         # Configurações da janela
-        self.setWindowFlags(Qt.FramelessWindowHint)  # Remove bordas da janela
         self.setWindowIcon(QIcon("icons/icons/logoSpike.png"))
         self.setGeometry(150, 200, 973, 437)
         
@@ -205,6 +269,7 @@ class AtasdeReuniao(TelaArrastavelBase):
         # Inicializar variáveis para armazenar referências às janelas
         self.tela_grafico_de_presenca = None
         self.tela_login = None
+        self.tela_catalogo_biblico = None
         
         print("✅ Tela Principal criada com arrasta e solta!")
 
@@ -212,7 +277,7 @@ class AtasdeReuniao(TelaArrastavelBase):
     def conectar_botoes(self):
         try:
             # BOTÕES PRINCIPAIS
-            self.ui.BTN_BUSCAR_MEMBRO.clicked.connect(self.abrir_tela_buscar_membro)
+            self.ui.BTN_BUSCAR_MEMBRO.clicked.connect(self.abrir_tela_catalogo_biblico)
             self.ui.BTN_GERAR_RELATORIO.clicked.connect(self.abrir_tela_login)
             self.ui.BTN_GRAFICO_DE_PRESENCA.clicked.connect(self.abrir_tela_grafico_de_presenca)
             
@@ -225,9 +290,9 @@ class AtasdeReuniao(TelaArrastavelBase):
 
     def configurar_shortcuts_principal(self):
         try:
-            # F3 - Abrir tela de busca de membros
+            # F3 - Abrir tela do catálogo bíblico
             self.shortcut_equipamentos = QShortcut(QKeySequence(Qt.Key_F3), self)
-            self.shortcut_equipamentos.activated.connect(self.abrir_tela_buscar_membro)
+            self.shortcut_equipamentos.activated.connect(self.abrir_tela_catalogo_biblico)
             
             # F4 - Abrir tela de gráficos
             self.shortcut_graficos = QShortcut(QKeySequence(Qt.Key_F4), self)
@@ -256,7 +321,7 @@ class AtasdeReuniao(TelaArrastavelBase):
         atalhos = """
     📋 ATALHOS DE TECLADO DISPONÍVEIS:
     F1 - Mostrar esta ajuda
-    F3 - Tela de busca de membros por nome
+    F3 - Catálogo Bíblico
     F4 - Tela de gráficos de presença
     ESC - Sair do sistema
     
@@ -265,14 +330,18 @@ class AtasdeReuniao(TelaArrastavelBase):
     """
         QMessageBox.information(self, "Atalhos do Sistema", atalhos)
     
-    def abrir_tela_buscar_membro(self):
+    def abrir_tela_catalogo_biblico(self):
         try:
-            QMessageBox.information(self, "Informação", 
-                                  "Funcionalidade de busca de membros\nem desenvolvimento!")
+            # Fechar janela anterior se existir
+            if self.tela_catalogo_biblico:
+                self.tela_catalogo_biblico.close()
+            self.tela_catalogo_biblico = CatalogoBiblico(self)
+            self.tela_catalogo_biblico.show()
+            print("✅ Tela Catálogo Bíblico aberta!")
         except Exception as e:
             QMessageBox.critical(self, "Erro", 
-                               f"Erro ao abrir tela de busca: {str(e)}")
-    
+                               f"Erro ao abrir catálogo bíblico: {str(e)}")
+
     def abrir_tela_grafico_de_presenca(self):
         try:
             # Fechar janela anterior se existir
@@ -281,7 +350,7 @@ class AtasdeReuniao(TelaArrastavelBase):
             
             self.tela_grafico_de_presenca = TelaGraficodePresenca(self)
             self.tela_grafico_de_presenca.show()
-            print("✅ Tela de gráfico aberta - pode ser arrastada separadamente!")
+            print("✅ Tela de gráfico aberta!")
         except Exception as e:
             QMessageBox.critical(self, "Erro", 
                                f"Erro ao abrir tela gráfico de presença: {str(e)}")
@@ -291,20 +360,21 @@ class AtasdeReuniao(TelaArrastavelBase):
             # Fechar janela anterior se existir
             if self.tela_login:
                 self.tela_login.close()
-            
             self.tela_login = Login(self)
             self.tela_login.show()
-            print("✅ Tela de login aberta - pode ser arrastada separadamente!")
-            
+            print("✅ Tela de login aberta!")
         except Exception as e:
             QMessageBox.critical(self, "Erro", 
                                f"Erro ao abrir tela de login: {str(e)}")
     
     def closeEvent(self, event):
+        # Fechar todas as janelas filhas
         if self.tela_grafico_de_presenca:
             self.tela_grafico_de_presenca.close()
         if self.tela_login:
             self.tela_login.close()
+        if self.tela_catalogo_biblico:
+            self.tela_catalogo_biblico.close()
         event.accept()
 
 
@@ -329,7 +399,7 @@ if __name__ == "__main__":
             main_window = AtasdeReuniao()
             main_window.show()
             print("🔧 Tela Principal criada - Teste arrastar ela também!")
-            print("💡 Dica: Abra outras telas (F3 Tela de busca de membros por nome), (F4 para gráfico), e arraste cada uma separadamente!")
+            print("💡 Dica: Abra outras telas (F3 Catálogo Bíblico), (F4 para gráfico), e arraste cada uma separadamente!")
             
             # Executar novamente para a tela principal
             sys.exit(app.exec_())
